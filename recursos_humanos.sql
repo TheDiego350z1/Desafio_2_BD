26c479b21fdc16f59248c5e8a29ef52643385fcf
Diego García <diego350z1@gmail.com>
2022-04-07 21:52:05 -0600

    Sentencias DDL (creación de base de datos, tablas, alteración, fk, pk)

--- recursos_humanos.sql
+++ recursos_humanos.sql
@@ -23,3 +23,43 @@ CREATE TABLE locations (
 	state_province VARCHAR(255),
 	country_id INT FOREIGN KEY REFERENCES countries(countries) 
 );
+
+CREATE TABLE jobs(
+	job_id INT PRIMARY KEY,
+	job_title VARCHAR(255),
+	min_salary MONEY,
+	max_salary MONEY,
+);
+
+CREATE TABLE employess (
+	employee_id INT PRIMARY KEY,
+	frist_name varchar(30),
+	last_name varchar(30),
+	email VARCHAR(255) UNIQUE,
+	phoNE VARCHAR(15),
+	hire_number varchar(15),
+	hire_date DATETIME,
+	salary MONEY,
+	commission_pct MONEY,
+	departament_id INT,
+	job_id INT FOREIGN KEY REFERENCES jobs(job_id),
+	manager_id INT FOREIGN KEY REFERENCES employess(employee_id),
+	--departament_id INT FOREIGN KEY REFERENCES departaments(departament_id),
+);
+
+CREATE TABLE departaments (
+	departament_id INT PRIMARY KEY,
+	departamen_name VARCHAR(255),
+	manager_id INT FOREIGN KEY REFERENCES employess(employee_id),
+	location_id INT FOREIGN KEY REFERENCES locations(location_id)
+);
+
+CREATE TABLE job_history (
+	job_historiy_id INT PRIMARY KEY,
+	start_date DATETIME NOT NULL,
+	end_date DATETIME NOT NULL,
+	job_id INT FOREIGN KEY REFERENCES jobs(job_id),
+	departament_id INT FOREIGN KEY REFERENCES departaments(departament_id),
+); 
+
+ALTER TABLE employess ADD CONSTRAINT FK_EMPLOYESS_DEPARTAMENTS FOREIGN KEY (departament_id) REFERENCES departaments(departament_id);
