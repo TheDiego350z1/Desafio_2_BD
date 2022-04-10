75f0230862ee9521ca9d34e9dcbc764854cb7454
Diego García <diego350z1@gmail.com>
2022-04-09 21:05:23 -0600

    Actualización PK para que sea auto increment

--- recursos_humanos.sql
+++ recursos_humanos.sql
@@ -5,18 +5,18 @@ USE recursos_humanos;
 
 --Creación de tablas
 CREATE TABLE regions (
-	region_id INT PRIMARY KEY,
+	region_id INT IDENTITY(1,1) PRIMARY KEY,
 	region_name VARCHAR(255)
 );
 
 CREATE TABLE countries (
-	countries INT PRIMARY KEY,
+	countries INT IDENTITY(1,1) PRIMARY KEY,
 	country_name VARCHAR(255),
 	region_id INT FOREIGN KEY REFERENCES regions(region_id)
 );
 
 CREATE TABLE locations (
-	location_id INT PRIMARY KEY,
+	location_id INT IDENTITY(1,1) PRIMARY KEY,
 	streed_address VARCHAR(255),
 	POSTAL_CODE VARCHAR(10), --se toma referencia https://www.azcodigopostal.com/
 	city VARCHAR(255),
@@ -25,14 +25,14 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE jobs(
-	job_id INT PRIMARY KEY,
+	job_id INT IDENTITY(1,1) PRIMARY KEY,
 	job_title VARCHAR(255),
 	min_salary MONEY,
 	max_salary MONEY,
 );
 
 CREATE TABLE employess (
-	employee_id INT PRIMARY KEY,
+	employee_id INT IDENTITY(1,1) PRIMARY KEY,
 	frist_name varchar(30),
 	last_name varchar(30),
 	email VARCHAR(255) UNIQUE,
@@ -48,14 +48,14 @@ CREATE TABLE employess (
 );
 
 CREATE TABLE departaments (
-	departament_id INT PRIMARY KEY,
+	departament_id INT IDENTITY(1,1) PRIMARY KEY,
 	departamen_name VARCHAR(255),
 	manager_id INT FOREIGN KEY REFERENCES employess(employee_id),
 	location_id INT FOREIGN KEY REFERENCES locations(location_id)
 );
 
 CREATE TABLE job_history (
-	job_historiy_id INT PRIMARY KEY,
+	job_historiy_id INT IDENTITY(1,1) PRIMARY KEY,
 	start_date DATETIME NOT NULL,
 	end_date DATETIME NOT NULL,
 	job_id INT FOREIGN KEY REFERENCES jobs(job_id),
@@ -63,3 +63,7 @@ CREATE TABLE job_history (
 ); 
 
 ALTER TABLE employess ADD CONSTRAINT FK_EMPLOYESS_DEPARTAMENTS FOREIGN KEY (departament_id) REFERENCES departaments(departament_id);
+
+--Inserciíon de datos 
+
+INSERT INTO regions VALUES('REGION 1');
